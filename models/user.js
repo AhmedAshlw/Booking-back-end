bd3e870f21f6253ebf7690a2593f49141a01da9a
Mueen <hejiy2002@gmail.com>
2024-08-18 09:04:46 +0300

    updating user schema

--- models/user.js
+++ models/user.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-  username: String,
-  hashedPassword: String,
-  isAdmin: Boolean,
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+  hashedPassword: { type: String, required: true },
+  isRestaurant: { type: Boolean, default: false },
+  isAdmin: { type: Boolean, default: false },
 });
 
 userSchema.set("toJSON", {
