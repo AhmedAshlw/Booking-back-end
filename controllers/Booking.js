78fa24aa9b47e1b70a736fbeae71d9b6b27fd028
Mueen <hejiy2002@gmail.com>
2024-08-19 18:42:41 +0300

    adding route for user viewing all his booking

--- controllers/Booking.js
+++ controllers/Booking.js
@@ -6,5 +6,14 @@ const Booking = require("../models/Booking");
 // protected Routes
 router.use(verifyToken);
 // route for viewing user Bookings
-
+router.get("/", async (req, res) => {
+  try {
+    const Bookings = await Booking.find({ userId: req.user._id }).sort({
+      createdAt: "desc",
+    });
+    res.status(200).json(Bookings);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 module.exports = router;
