4b1ee84ac59730ee2f6250c04857e196160d52d8
Mueen <hejiy2002@gmail.com>
2024-08-19 12:51:09 +0300

    ,

--- models/Booking.js
+++ models/Booking.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 const BookingSchema = new mongoose.Schema({
   restaurantId: { type: mongoose.Schema.Types.ObjectId, ref: "restaurant" },
-
-  date: Date,
-  time: String,
+  Seats: { type: Number, required: true },
+  date: { type: Date, required: true },
+  time: { type: String, required: true },
 
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
 });
