b17bce019d5d9d98a12103f88c5db4a679f2f2c7
Mueen <hejiy2002@gmail.com>
2024-08-20 09:45:30 +0300

    .

--- controllers/Booking.js
+++ controllers/Booking.js
@@ -6,11 +6,14 @@ const Booking = require("../models/Booking");
 // protected Routes
 router.use(verifyToken);
 // route for viewing user Bookings
+
 router.get("/", async (req, res) => {
   try {
-    const Bookings = await Booking.find({ userId: req.user._id }).sort({
-      createdAt: "desc",
-    });
+    const Bookings = await Booking.find({ userId: req.user._id })
+      .populate("restaurantId")
+      .sort({
+        createdAt: "desc",
+      });
     res.status(200).json(Bookings);
   } catch (error) {
     res.status(500).json(error);
