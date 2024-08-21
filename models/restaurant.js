368e263f4019d52f1cf39a9dfc9218a54843eb97
Mueen <hejiy2002@gmail.com>
2024-08-21 14:30:24 +0300

    mahmood edited the restaurant schema to add picture

--- models/restaurant.js
+++ models/restaurant.js
@@ -1,12 +1,9 @@
 const mongoose = require("mongoose");
-
 const User = require("../models/user");
-
 const ratingSchema = new mongoose.Schema({
   rate: { type: Number },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
 });
-
 const commentSchema = new mongoose.Schema(
   {
     text: {
@@ -17,14 +14,12 @@ const commentSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
-
 const restaurantSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
   },
   owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-
   category: {
     type: String,
     required: true,
@@ -33,8 +28,10 @@ const restaurantSchema = new mongoose.Schema({
   rating: [ratingSchema],
   operatingHours: String,
   comments: [commentSchema],
+  resimage: {
+    type: String,
+    required: false,
+  },
 });
-
 const Restaurant = mongoose.model("restaurant", restaurantSchema);
-
 module.exports = Restaurant;
